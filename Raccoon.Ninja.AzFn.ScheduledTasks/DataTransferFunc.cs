2e924f6f193bd41fb296ed063aa2cc13717e770d
Breno RdV <noreply@raccoon.ninja>
2024-06-23 15:28:31 -0400

    Forgot to uncomment one of the functions.

--- Raccoon.Ninja.AzFn.ScheduledTasks/DataTransferFunc.cs
+++ Raccoon.Ninja.AzFn.ScheduledTasks/DataTransferFunc.cs
@@ -13,7 +13,6 @@ using Raccoon.Ninja.Extensions.MongoDb.Models;
 
 namespace Raccoon.Ninja.AzFn.ScheduledTasks;
 
-
 public class DataTransferFunc
 {
     private readonly ILogger _logger;
@@ -23,61 +22,63 @@ public class DataTransferFunc
         _logger = logger;
     }
 
-    // [Function("DataTransferFunc")]
-    // [CosmosDBOutput(
-    //     "%CosmosDatabaseName%",
-    //     "%CosmosContainerName%", 
-    //     Connection = "CosmosConnectionString",
-    //     CreateIfNotExists = false)]
-    // public IEnumerable<GlucoseReading> Run(
-    //     [TimerTrigger("0 */5 * * * *", RunOnStartup = true)] TimerInfo timer, 
-    //     [CosmosDBInput(
-    //         databaseName: "%CosmosDatabaseName%", 
-    //         containerName: "%CosmosContainerName%",
-    //         Connection = "CosmosConnectionString",
-    //         SqlQuery = "SELECT TOP 1 * FROM c ORDER BY c.readAt DESC"
-    //     )] IEnumerable<GlucoseReading> previousReadings)
-    // {
-    //     _logger.LogInformation("Nightscout Data Transfer Function started");
-    //
-    //     try
-    //     {
-    //         var previousReading = previousReadings.FirstOrDefault();
-    //
-    //         var targetTimestamp = previousReading?.ReadTimestampUtc ?? 0;
-    //
-    //         var collection = GetMongoCollection(_logger);
-    //
-    //         var documents = collection.GetDocumentsSince(targetTimestamp);
-    //
-    //         if (!documents.HasElements())
-    //         {
-    //             _logger.LogWarning("No documents to transfer");
-    //
-    //             return new List<GlucoseReading>();
-    //         }
-    //
-    //         var glucoseReadings = documents.ToGlucoseReadings(previousReading);
-    //
-    //         _logger.LogInformation("Converted {Count} documents to CosmosDb", documents.Count);
-    //
-    //         return glucoseReadings;
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         _logger.LogError(e, "Failed to transfer data from MongoDb to CosmosDb");
-    //
-    //         throw;
-    //     }
-    //     finally
-    //     {
-    //         _logger.LogTrace("Nightscout Data Transfer Function finished!");
-    //     }
-    // }
+    [Function("DataTransferFunc")]
+    [CosmosDBOutput(
+        "%CosmosDatabaseName%",
+        "%CosmosContainerName%",
+        Connection = "CosmosConnectionString",
+        CreateIfNotExists = false)]
+    public IEnumerable<GlucoseReading> Run(
+        [TimerTrigger("0 */5 * * * *", RunOnStartup = true)]
+        TimerInfo timer,
+        [CosmosDBInput(
+            "%CosmosDatabaseName%",
+            "%CosmosContainerName%",
+            Connection = "CosmosConnectionString",
+            SqlQuery = "SELECT TOP 1 * FROM c ORDER BY c.readAt DESC"
+        )]
+        IEnumerable<GlucoseReading> previousReadings)
+    {
+        _logger.LogInformation("Nightscout Data Transfer Function started");
+
+        try
+        {
+            var previousReading = previousReadings.FirstOrDefault();
+
+            var targetTimestamp = previousReading?.ReadTimestampUtc ?? 0;
+
+            var collection = GetMongoCollection(_logger);
+
+            var documents = collection.GetDocumentsSince(targetTimestamp);
+
+            if (!documents.HasElements())
+            {
+                _logger.LogWarning("No documents to transfer");
+
+                return new List<GlucoseReading>();
+            }
+
+            var glucoseReadings = documents.ToGlucoseReadings(previousReading);
+
+            _logger.LogInformation("Converted {Count} documents to CosmosDb", documents.Count);
+
+            return glucoseReadings;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to transfer data from MongoDb to CosmosDb");
+
+            throw;
+        }
+        finally
+        {
+            _logger.LogTrace("Nightscout Data Transfer Function finished!");
+        }
+    }
 
     /// <summary>
-    /// Initializes the MongoDb collection.
-    /// This is the source of data for Nightscout.
+    ///     Initializes the MongoDb collection.
+    ///     This is the source of data for Nightscout.
     /// </summary>
     /// <param name="log">Log instance created by Azure.</param>
     /// <returns>Collection Instance</returns>
