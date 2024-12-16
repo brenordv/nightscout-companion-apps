0b5f12a09dc39a81784912ddc03166d4f231a828
Breno RdV <brenordv@users.noreply.github.com>
2024-12-16 03:02:03 -0500

    Changed the function that gets the latest glucose reading to return a list with the 'latest since'.

--- Raccoon.Ninja.AzFn.DataApi/DataApiFunc.cs
+++ Raccoon.Ninja.AzFn.DataApi/DataApiFunc.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Raccoon.Ninja.AzFn.DataApi.ExtensionMethods;
 using Raccoon.Ninja.AzFn.DataApi.Utils;
 using Raccoon.Ninja.Domain.Core.Entities;
 using Raccoon.Ninja.Domain.Core.Models;
-using Microsoft.Azure.Functions.Worker;
-using Newtonsoft.Json;
 
 namespace Raccoon.Ninja.AzFn.DataApi;
 
@@ -26,39 +26,71 @@ public class DataApiFunc
     [Function("DataApiFunc")]
     public async Task<IActionResult> RunAsync(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
-        HttpRequest req, 
+        HttpRequest req,
         [CosmosDBInput(
-            databaseName: "%CosmosDatabaseName%", 
-            containerName: "%CosmosContainerName%",
-            Connection = "CosmosConnectionString",
-            SqlQuery = "SELECT TOP 1 * FROM c ORDER BY c.readAt DESC"
+            "%CosmosDatabaseName%",
+            "%CosmosContainerName%",
+            Connection = "CosmosConnectionString"
         )]
-        IEnumerable<GlucoseReading> previousReadings)
+        Container container)
     {
-        _logger.LogInformation("Data API call received. Request by IP: {Ip}", req.HttpContext.Connection.RemoteIpAddress);
+        _logger.LogInformation("Data API call received. Request by IP: {Ip}",
+            req.HttpContext.Connection.RemoteIpAddress);
 
-        GlucoseReading latestReading = null;
+        var results = new List<GlucoseReadingResponse>();
 
         try
         {
             if (!Validators.IsKeyValid(await req.Body.ExtractKey()))
                 return new UnauthorizedResult();
 
-            latestReading = previousReadings.FirstOrDefault();
+            var readSince = req.TryGetReadSinceParam();
 
-            GlucoseReadingResponse response = latestReading;
+            if (readSince.HasValue)
+            {
+                // Build a parameterized query
+                var queryDefinition = new QueryDefinition(
+                    "SELECT * FROM c WHERE c.readAt > @readSince ORDER BY c.readAt DESC"
+                ).WithParameter("@readSince", readSince);
 
-            return latestReading is null
-                ? new NoContentResult()
-                : new OkObjectResult(response);
+
+                using var iteratorReadsince = container.GetItemQueryIterator<GlucoseReading>(queryDefinition);
+                while (iteratorReadsince.HasMoreResults)
+                {
+                    var response = await iteratorReadsince.ReadNextAsync();
+                    results.AddRange(response.Select(item => (GlucoseReadingResponse)item));
+                }
+
+                return results.Count != 0 ? new OkObjectResult(results) : new NoContentResult();
+            }
+
+            // If no readSince parameter is provided, return the latest reading
+            using var iteratorLatestReading = container.GetItemQueryIterator<GlucoseReading>(
+                new QueryDefinition("SELECT TOP 1 * FROM c ORDER BY c.readAt DESC")
+            );
+
+            while (iteratorLatestReading.HasMoreResults)
+            {
+                var response = await iteratorLatestReading.ReadNextAsync();
+                var latestReading = response.FirstOrDefault();
+
+                if (latestReading is not null)
+                {
+                    results.Add(latestReading);
+                    return new OkObjectResult(results);
+                }
+
+                break;
+            }
+
+            return new NoContentResult();
         }
         catch (Exception e)
         {
             _logger.LogError(e, "Error while processing request from IP: {Ip} | Latest reading : {LatestReading}",
-                            req.HttpContext.Connection.RemoteIpAddress, latestReading);
+                req.HttpContext.Connection.RemoteIpAddress, results);
 
             return new StatusCodeResult(500);
         }
     }
-
 }
